adefb9f07eec6cb33eb7543376a79cea7de1814e
Nikola Sirakov <nikolasirakov1@gmail.com>
2025-10-22 22:06:25 +0300

    Add HomeSlider component with Swiper integration and update styles; include new images for the slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomeSlider from "@/components/HomeSlider";
 
 export default function Home() {
   return (
     <div>
-      
+      <HomeSlider />
     </div>
   );
 }
---
+++ src/components/HomeSlider.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Image from 'next/image';
+import { Navigation } from 'swiper/modules';
+
+const HomeSlider = () => {
+  return (
+    <div className='homeSlider'>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+          <div className='item'>
+            <Image src={"/hero_slider/nike-1.png"} alt='Nike Slide 1' width={1344} height={514} className='mx-6 mt-6'/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='item'>
+            <Image src={"/hero_slider/nike-2.png"} alt='Nike Slide 2' width={1344} height={514} className='mx-6 mt-6'/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='item'>
+            <Image src={"/hero_slider/nike-3.png"} alt='Nike Slide 3' width={1344} height={514} className='mx-6 mt-6'/>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
+
+export default HomeSlider
