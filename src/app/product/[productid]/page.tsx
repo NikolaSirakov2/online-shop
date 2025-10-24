2cc4852902b0d3462da3b4d40c555e9bbb15c9b0
Nikola Sirakov <nikolasirakov1@gmail.com>
2025-10-24 06:34:27 +0300

    Add ProductPageLayout and ProductDetails components for product detail page; establish layout structure and basic content display.

---
+++ src/app/product/[productid]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <section>
+      <div className='container'>
+        ProductDetails
+      </div>
+    </section>
+  )
+}
+
+export default ProductDetails
